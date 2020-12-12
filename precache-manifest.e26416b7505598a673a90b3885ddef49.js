8bd2d7bf8b8311aaf26e24a4143f74ed9cb789e7
vaibhav jadhav <vaibhav.jadhav.csm@gmail.com>
2020-12-12 15:38:24 +0530

    Updates

---
+++ precache-manifest.e26416b7505598a673a90b3885ddef49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4a47242655b83812c7988d97357ae7",
+    "url": "/diploydemo/index.html"
+  },
+  {
+    "revision": "b21e4cf7fe4eca0a2f2c",
+    "url": "/diploydemo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "24ef72953096de2a33b2",
+    "url": "/diploydemo/static/js/2.8d1a3f2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diploydemo/static/js/2.8d1a3f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21e4cf7fe4eca0a2f2c",
+    "url": "/diploydemo/static/js/main.178df6d8.chunk.js"
+  },
+  {
+    "revision": "50f35cffdd8d8306ebe7",
+    "url": "/diploydemo/static/js/runtime-main.b5344e62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/diploydemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
